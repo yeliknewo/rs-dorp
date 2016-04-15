2da41d50be7c09f8446014c50ef75e5a8c29ca46
Kiley Owen <yelikdev@gmail.com>
2016-04-15 16:11:47 -0400

    Added Syncdata to scene

--- src/components/scene.rs
+++ src/components/scene.rs
@@ -1,20 +1,21 @@
 use std::error::Error;
 
 use logic::{IdManager, World, Entity, Id};
+use graphics::{SyncData};
 
 pub struct Scene<T: Entity<T>> {
-    setup: Box<Fn(&mut IdManager, &mut World<T>) -> Result<(), Box<Error>> + Send + Sync>,
+    setup: Box<Fn(&mut IdManager, &mut World<T>, &mut SyncData) -> Result<(), Box<Error>> + Send + Sync>,
 }
 
 impl<T: Entity<T>> Scene<T> {
-    pub fn new(setup: Box<Fn(&mut IdManager, &mut World<T>) -> Result<(), Box<Error>> + Send + Sync>) -> Scene<T> {
+    pub fn new(setup: Box<Fn(&mut IdManager, &mut World<T>, &mut SyncData) -> Result<(), Box<Error>> + Send + Sync>) -> Scene<T> {
         Scene {
             setup: setup,
         }
     }
 
-    pub fn tick_mut(&mut self, my_id: Id, manager: &mut IdManager, world: &mut World<T>) -> Result<(), Box<Error>> {
+    pub fn tick_mut(&mut self, my_id: Id, manager: &mut IdManager, world: &mut World<T>, sync_data: &mut SyncData) -> Result<(), Box<Error>> {
         world.queue_remove_entity(my_id);
-        (self.setup)(manager, world)
+        (self.setup)(manager, world, sync_data)
     }
 }
